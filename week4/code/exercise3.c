d49dc8ec846ce6eab6f90dba491574eac91f3a80
Lars Dahl <laerdh@gmail.com>
2016-09-21 20:46:22 +0100

    Completed tasks for week 4

--- week4/code/exercise3.c
+++ week4/code/exercise3.c
@@ -1,23 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+// Function declaration (for Compiler)
+void swap();
+void printArray();
+
+
 int main() {
-	int *arrayInt = (int *)malloc(1*sizeof(int));
+	int *p_array = (int *)malloc(1*sizeof(int));
 	int i;
 	int count;
 
-	while(i != 0) {
+	while(1) {
 		printf("Please enter a number\n");
 		scanf("%d", &i);
-		arrayInt[count] = i; 
-		count++;
-		arrayInt = (int *)realloc(arrayInt, ((count+1)*sizeof(int)));
+		if (i == 0) break;
+		p_array[count++] = i; 
+		p_array = (int *)realloc(p_array, ((count+1)*sizeof(int)));
 	}
 
-	free(arrayInt);
-	arrayInt = NULL;
-
 	// Do Bubblesort algorithm here
+	for (int n = 0; n < (count-1); n++) {
+		for (int k = 0; k < (count-1); k++) {
+			if (p_array[k] > p_array[k+1]) {
+				swap(p_array, k, k+1);
+			}
+		}
+	}
+
+	// Print array
+	printArray(p_array, count);
+
+	// Free memory
+	free(p_array);
+	p_array = NULL;
 
 	return 0;
+}
+
+
+// Function definitions
+void swap(int *array, int a, int b) {
+	int temp = array[a];
+	array[a] = array[b];
+	array[b] = temp;
+
+	return;
+}
+
+void printArray(int *array, int size) {
+	printf("List is sorted:\n");
+	for (int i = 0; i < size; i++) {
+		printf("[%d]", array[i]);
+	}
+	printf("\n");
+
+	return;
 }
